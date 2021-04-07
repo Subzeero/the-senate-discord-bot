a6a525efe5ab8e7ad8acbdec52aa5850864bb2f2
repl.it user <replituser@example.com>
2021-04-07 07:41:37 +0000

    Fix online database edit command.

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 import database as db
-#View DB:
-#curl "https://databasemanager.ironblockhd.repl.co/g/" --get -d $REPLIT_DB_URL
+# View DB:
+# curl "https://databasemanager.ironblockhd.repl.co/g/" --get -d $REPLIT_DB_URL
 
 class Admin(commands.Cog):
 	"""Administrator commands."""
