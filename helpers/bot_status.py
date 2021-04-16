9f74922aa35a47f1ae1d253c8bb5468ca4768691
Subzeero <zeerommelaere@gmail.com>
2021-04-15 19:42:22 -0600

    Fix references to statusReference

--- helpers/bot_status.py
+++ helpers/bot_status.py
@@ -23,12 +23,12 @@ async def update_status():
 	if status_data["maintenance"]:
 		await discord.Client.change_presence(
 			activity = discord.Activity(type = activityReference["playing"], name = "maintenance mode."),
-			status = activityReference["dnd"]
+			status = statusReference["dnd"]
 		)
 	else:
 		await discord.Client.change_presence(
 				activity = discord.Activity(type = activityReference[status_data["activity"]], name = status_data["message"]),
-				status = activityReference[status_data["status"]]
+				status = statusReference[status_data["status"]]
 			)
 
 def get_reference(referenceType):
