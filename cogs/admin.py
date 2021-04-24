0e30df34267ab2b39c0cb1690dd598a38abaef18
Subzeero <zeerommelaere@gmail.com>
2021-04-24 11:57:25 -0600

    Add commands for echoing and editing the echos (with embeds)

--- cogs/admin.py
+++ cogs/admin.py
@@ -41,6 +41,62 @@ class Admin(commands.Cog):
 
 	@commands.command()
 	@commands.is_owner()
+	async def editMessage(self, ctx, messageID:int, newContent:str):
+		"""Edit a message from the bot."""
+
+		await ctx.message.delete()
+
+		try:
+			message = ctx.fetch_message(messageID)
+		except discord.NotFound:
+			await ctx.send(content = f"❌ Invalid messageID: `{messageID}`!", delete_after = 3)
+			return
+
+		if message.author.id == self.user.id:
+			await ctx.send(content = f"❌ Invalid messageID: `{messageID}`!", delete_after = 3)
+			return
+
+		await message.edit(content = newContent, embed = None)
+
+	@commands.command()
+	@commands.is_owner()
+	async def echoEmbed(self, ctx, channel:discord.TextChannel, *, content:str):
+		"""Make an announcement."""
+
+		embed = discord.Embed(
+			description = content,
+			colour = discord.Colour.gold()
+		)
+
+		await ctx.message.delete()
+		await channel.send(embed = embed)
+
+	@commands.command()
+	@commands.is_owner()
+	async def editEmbedMessage(self, ctx, messageID:int, *, newContent:str):
+		"""Edit a message with an embed."""
+
+		await ctx.message.delete()
+
+		try:
+			message = ctx.fetch_message(messageID)
+		except discord.NotFound:
+			await ctx.send(content = f"❌ Invalid messageID: `{messageID}`!", delete_after = 3)
+			return
+
+		if message.author.id == self.user.id:
+			await ctx.send(content = f"❌ Invalid messageID: `{messageID}`!", delete_after = 3)
+			return
+
+		embed = discord.Embed(
+			description = newContent,
+			colour = discord.Colour.gold()
+		)
+
+		await message.edit(content = None, embed = embed)
+
+	@commands.command()
+	@commands.is_owner()
 	async def changeStatusType(self, ctx, newStatusStr):
 		"""Change the bot's status type.
 		Valid status types: `online`, `idle`, `dnd`, `invisible`."""
