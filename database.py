8062bcad63c61b3afd32bb09f9ec173c351cd12d
Subzeero <zeerommelaere@gmail.com>
2021-05-03 15:09:42 -0600

    Database fixes; new servers and key checks

--- database.py
+++ database.py
@@ -4,15 +4,23 @@ from replit import db
 
 def validate_server(serverId: int):
 	serverId = str(serverId)
-	server_data = {
+	data_template = {
 		"reaction_roles": [],
-		"custom_roles": {}
+		"custom_roles": {},
+		"auto_reactions": {},
+		"DATA_VERSION": 1
 	}
 
 	if not serverId in db.keys():
+		server_data = data_template
 		db[serverId] = server_data
+
 	else:
 		server_data = db[serverId]
+		for key, value in data_template.items():
+			if not key in server_data:
+				server_data[key] = value
+		db[serverId] = server_data
 
 	return server_data
 
@@ -32,4 +40,3 @@ def set(key, value):
 
 def get_keys():
 	return db.keys()
-	
