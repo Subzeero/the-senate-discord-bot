6287480641fc791cd8ccc6ee390fd005550f1c63
Subzeero <zeerommelaere@gmail.com>
2021-05-04 00:00:22 -0600

    Phase 1 of unserverlocked admin

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from database import db
 
 class Admin(commands.Cog):
 	"""Server administrator commands."""
@@ -7,8 +8,50 @@ class Admin(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
+	def checkAdminPerm(ctx):
+		return ctx.channel.permissions_for(ctx.author).administrator
+
+	def checkAdminRole(ctx):
+		valid_roles = db.validate_server(ctx.guild.id)["admin_roles"]
+		for role in ctx.author.roles:
+			if str(role) in valid_roles or str(role.id) in valid_roles:
+				return True
+		return False
+
+	@commands.command()
+	@commands.guild_only()
+	@commands.check_any(checkAdminPerm, checkAdminRole)
+	async def listControlRoles(self, ctx):
+		"""List the admin and mod roles used by the bot."""
+
+		server_data = db.validate_server(ctx.guild.id)
+		admin_roles = server_data["admin_roles"]
+		mod_roles = server_data["moderator_roles"]
+
+		embed = discord.Embed(
+			colour = discord.Colour.gold()
+		)
+
+		embed.set_author(
+			name = f"Bot Control Roles in {ctx.guild.name}",
+			icon_url = ctx.guild.icon_url
+		)
+
+		embed.add_field(
+			name = "Admin Roles",
+			value = "\n".join(admin_roles)
+		)
+
+		embed.add_field(
+			name = "Moderator Roles",
+			value = "\n".join(mod_roles)
+		)
+
+		await ctx.send(embed = embed)
+
 	@commands.command(aliases = ["say"])
-	@commands.is_owner()
+	@commands.guild_only()
+	@commands.check_any(checkAdminPerm, checkAdminRole)
 	async def echo(self, ctx, *, content:str):
 		"""Echo a message back from the bot."""
 
@@ -16,7 +59,8 @@ class Admin(commands.Cog):
 		await ctx.send(content)
 
 	@commands.command(aliases = ["edit"])
-	@commands.is_owner()
+	@commands.guild_only()
+	@commands.check_any(checkAdminPerm, checkAdminRole)
 	async def editMessage(self, ctx, messageID:int, newContent:str):
 		"""Edit a message from the bot."""
 
@@ -35,7 +79,8 @@ class Admin(commands.Cog):
 		await message.edit(content = newContent, embed = None)
 
 	@commands.command(aliases = ["sayembed"])
-	@commands.is_owner()
+	@commands.guild_only()
+	@commands.check_any(checkAdminPerm, checkAdminRole)
 	async def echoEmbed(self, ctx, channel:discord.TextChannel, *, content:str):
 		"""Make an announcement."""
 
@@ -48,7 +93,8 @@ class Admin(commands.Cog):
 		await channel.send(embed = embed)
 
 	@commands.command()
-	@commands.is_owner()
+	@commands.guild_only()
+	@commands.check_any(checkAdminPerm, checkAdminRole)
 	async def editEmbedMessage(self, ctx, messageID:int, *, newContent:str):
 		"""Edit a message with an embed."""
 
