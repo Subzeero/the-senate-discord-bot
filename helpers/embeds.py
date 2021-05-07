217553caecc25b0713d45622310263faff494fef
Subzeero <zeerommelaere@gmail.com>
2021-05-07 17:33:32 -0600

    Create embeds.py

--- helpers/embeds.py
+++ helpers/embeds.py
@@ -1,1 +1,46 @@
-# TBD
+import discord
+
+def tempEmbed(*, name = None, desc = None, colour = discord.Colour.gold(), author = None):
+	embed = discord.Embed(
+		name = name,
+		description = desc,
+		colour = colour
+	)
+
+	embed.set_footer(text = "This message will self-destruct in 10 seconds.")
+	if author:
+		embed.set_author(
+			name = author.name + "#" + author.discriminator,
+			icon_url = author.icon_url
+		)
+	return embed
+
+def infoEmbed(*, name = None, desc = None, colour = discord.Colour.gold(), author = None, footer = None):
+	embed = discord.Embed(
+		name = name,
+		description = desc,
+		colour = colour
+	)
+	if author:
+		embed.set_author(
+			name = author.name,
+			icon_url = author.icon_url
+		)
+	if footer:
+		embed.set_footer(text = footer)
+	return embed
+
+def customEmbed(*, name = None, desc = None, colour = discord.Colour.gold(), authorName = None, authorIconURL = None, footer = None):
+	embed = discord.Embed(
+		name = name,
+		description = desc,
+		colour = colour
+	)
+	if authorName or authorIconURL:
+		embed.set_author(
+			name = authorName,
+			icon_url = authorIconURL
+		)
+	if footer:
+		embed.set_footer(text = footer)
+	return embed
