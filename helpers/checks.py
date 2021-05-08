8a30bfc5ddc018729f610ab63e60693cc138e526
Subzeero <zeerommelaere@gmail.com>
2021-05-08 14:05:14 -0600

    Add admin and mod checks

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,1 +1,14 @@
-# TBD
+import discord
+from discord.ext import commands
+
+def isAdmin():
+	async def predicate(ctx):
+		valid_roles = db.validate_server(ctx.guild.id)["admin_roles"]
+		return commands.check_any(commands.has_permissions(administrator), commands.has_any_role(valid_roles))
+	return commands.check(predicate)
+
+def isMod():
+	async def predicate(ctx):
+		valid_roles = db.validate_server(ctx.guild.id)["mod_roles"]
+		return commands.has_any_role(valid_roles)
+	return predicate
