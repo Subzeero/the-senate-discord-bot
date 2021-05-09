38037f1b2c39e63924112c15365a320f9a1380f1
Subzeero <zeerommelaere@gmail.com>
2021-05-08 20:53:17 -0600

    Fix admin and mod checks

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,14 +1,21 @@
 import discord
 from discord.ext import commands
+import database as db
 
 def isAdmin():
 	async def predicate(ctx):
 		valid_roles = db.validate_server(ctx.guild.id)["admin_roles"]
-		return commands.check_any(commands.has_permissions(administrator), commands.has_any_role(valid_roles))
+		if not valid_roles:
+			return commands.has_permissions(administrator = True)
+		else:
+			return commands.has_permissions(administrator = True) or commands.has_any_role(valid_roles)
 	return commands.check(predicate)
 
 def isMod():
 	async def predicate(ctx):
 		valid_roles = db.validate_server(ctx.guild.id)["mod_roles"]
-		return commands.has_any_role(valid_roles)
+		if not valid_roles:
+			return False
+		else:
+			return commands.has_any_role(valid_roles)
 	return commands.check(predicate)
