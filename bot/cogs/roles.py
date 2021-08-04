1e8fe3cc97c3e458d3bdc3102432aedc0eb7d2ca
Subzeero <zeerommelaere@gmail.com>
2021-08-03 21:54:27 -0600

    Move 'user_permissions' from roles.py to users.py

--- bot/cogs/roles.py
+++ bot/cogs/roles.py
@@ -555,35 +555,5 @@ class roles(commands.Cog, name = "Roles"):
 
 		await ctx.send(embed = embed)
 
-	@commands.command(name = "userPermissions", aliases = ["uperms", "userperms"])
-	@commands.guild_only()
-	@commands.cooldown(1, 3, commands.BucketType.user)
-	async def user_permissions(self, ctx, member: discord.Member = None):
-		"""List the permissions of a given user."""
-
-		if not member:
-			member = ctx.author
-
-		perms = "\n".join(perm for perm, value in member.guild_permissions if value)
-
-		embed = discord.Embed(
-			title = "Permissions",
-			colour = member.colour
-		)
-
-		embed.set_author(
-			icon_url = member.avatar_url,
-			name = str(member)
-		)
-
-		embed.add_field(
-			name = "\uFEFF",
-			value = perms,
-			inline = False
-		)
-
-		await ctx.send(embed = embed)
-
-
 def setup(client):
 	client.add_cog(roles(client))
