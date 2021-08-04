1ae52cbf3da47bfbd81e976c6750c893522ebe48
Subzeero <zeerommelaere@gmail.com>
2021-08-03 21:53:32 -0600

    Basic restructure

--- bot/helpers/checks.py
+++ bot/helpers/checks.py
@@ -1,21 +1,20 @@
-import discord
 from discord.ext import commands
 from database.db import Database as db
 
-def isAdmin():
+def is_admin():
 	async def predicate(ctx):
 		valid_roles = db.get_guild(ctx.guild.id)["admin_roles"]
-		if not valid_roles:
-			return await commands.has_permissions(administrator = True).predicate(ctx)
-		else:
+		if valid_roles:
 			return await commands.has_permissions(administrator = True).predicate(ctx) or await commands.has_any_role(valid_roles).predicate(ctx)
+		else:
+			return await commands.has_permissions(administrator = True).predicate(ctx)
 	return commands.check(predicate)
 
-def isMod():
+def is_mod():
 	async def predicate(ctx):
 		valid_roles = db.get_guild(ctx.guild.id)["mod_roles"]
-		if not valid_roles:
-			return False
-		else:
+		if valid_roles:
 			return await commands.has_any_role(valid_roles).predicate(ctx)
+		else:
+			return False
 	return commands.check(predicate)
