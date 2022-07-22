3405d3311efca23e113a5acd6c2370760e3e5b11
Subzeero <76539691+Subzeero@users.noreply.github.com>
2022-07-22 10:47:18 -0600

    Rework TimeConverter to RelativeTimeConverter

--- bot/utils/converters.py
+++ bot/utils/converters.py
@@ -9,21 +9,26 @@ class UnicodeEmojiConverter(commands.Converter):
 		return emoji.emoji
 
 time_regex = re.compile(r"(\d{1,5}(?:[.,]?\d{1,5})?)([smhd])")
-time_dict = {"h":3600, "s":1, "m":60, "d":86400}
+time_dict = {"s":1, "m":60, "h":3600, "d":86400}
 whitespace_regex = re.compile(r'\s+')
 
-class TimeConverter(commands.Converter):
+class RelativeTimeConverter(commands.Converter):
 	async def convert(self, ctx, argument):
-		matches = time_regex.findall(whitespace_regex.sub("", argument.lower()))
 		time = 0
-		for num, determiner in matches:
-			try:
-				time += time_dict[determiner] * int(num)
-			except KeyError:
-				raise commands.BadArgument(f"`{determiner}` is invalid! Use `s`, `m`, `h`, or `d`.")
-			except ValueError:
-				raise commands.BadArgument(f"`{num}` in not a number!")
+		try:
+			num = int(argument)
+		except:
+			matches = time_regex.findall(whitespace_regex.sub("", argument.lower()))
+			for num, determiner in matches:
+				try:
+					time += time_dict[determiner] * int(num)
+				except KeyError:
+					raise commands.BadArgument(f"`{determiner}` is invalid! Use `s`, `m`, `h`, or `d`.")
+				except ValueError:
+					raise commands.BadArgument(f"`{num}` in not a number!")
+		else:
+			time = num
 
-		if time == 0:
-			raise commands.BadArgument(f"`{argument}` is not a valid time. Use `<num>` `<s | m | h | d>`.")
+		if time <= 0:
+			raise commands.BadArgument(f"`{argument}` is not a valid time. Use `<num> <s | m | h | d>`.")
 		return time
