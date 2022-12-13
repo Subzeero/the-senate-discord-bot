24a01dcc86cf136b38e4b61aefe1848234fcda6a
Subzeero <76539691+Subzeero@users.noreply.github.com>
2022-12-12 19:06:56 -0600

    Convert reaction roles cog to slash commands
    * Convert commands to slash
    * Update database access to properly await calls.

--- bot/cogs/reaction_roles.py
+++ bot/cogs/reaction_roles.py
@@ -1,24 +1,31 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-from typing import Union
 from database.db import Database as db
-from utils import checks, converters, find_object
+from utils import find_object, transformers
 
 class reaction_roles(commands.Cog, name = "Reaction Roles"):
 	"""Manage Reaction Roles."""
 
-	def __init__(self, client):
-		self.client = client
+	def __init__(self, bot: commands.Bot):
+		self.bot = bot
+
+	cmd_group = app_commands.Group(
+		name="reactionroles",
+		description="Reaction Role Commands.",
+		guild_only=(True),
+		default_permissions=discord.Permissions(administrator=True)
+	)
 
 	@commands.Cog.listener()
 	async def on_raw_reaction_add(self, payload):
-		if payload.event_type != "REACTION_ADD" or payload.user_id == self.client.user.id or not payload.guild_id:
+		if payload.event_type != "REACTION_ADD" or payload.user_id == self.bot.user.id or not payload.guild_id:
 			return
 
 		guild_id = payload.guild_id
 		message_id = payload.message_id
 		
-		guild_object = await find_object.find_guild(self.client, guild_id)
+		guild_object = await find_object.find_guild(self.bot, guild_id)
 		if not guild_object:
 			return
 
@@ -27,7 +34,7 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 			return
 
 		emoji_object = payload.emoji
-		guild_data = db.get_guild(guild_id)
+		guild_data = await db.get_guild(guild_id)
 		rr_data = None
 
 		for data in guild_data["reaction_roles"]:
@@ -40,7 +47,7 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 			if role_object:
 				await member_object.add_roles(role_object, reason = "Reaction Role")
 			else:
-				channel_object = await find_object.find_channel(self.client, rr_data["channel_id"])
+				channel_object = await find_object.find_channel(self.bot, rr_data["channel_id"])
 				try:
 					await channel_object.send(f"Reaction Role on message {message_id} has failed.\nThe role with id: `{rr_data['role_id']}` could not be found.")
 				except:
@@ -48,13 +55,13 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 
 	@commands.Cog.listener()
 	async def on_raw_reaction_remove(self, payload):
-		if payload.event_type != "REACTION_REMOVE" or payload.user_id == self.client.user.id or not payload.guild_id:
+		if payload.event_type != "REACTION_REMOVE" or payload.user_id == self.bot.user.id or not payload.guild_id:
 			return
 
 		guild_id = payload.guild_id
 		message_id = payload.message_id
 		
-		guild_object = await find_object.find_guild(self.client, guild_id)
+		guild_object = await find_object.find_guild(self.bot, guild_id)
 		if not guild_object:
 			return
 
@@ -63,7 +70,7 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 			return
 
 		emoji_object = payload.emoji
-		guild_data = db.get_guild(guild_id)
+		guild_data = await db.get_guild(guild_id)
 		rr_data = None
 
 		for data in guild_data["reaction_roles"]:
@@ -77,41 +84,36 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 		except:
 			pass
 
-	@commands.command(aliases = ["reactionRoles"])
-	@commands.guild_only()
-	@commands.cooldown(1, 10, commands.BucketType.guild)
-	@checks.is_admin_or_mod()
-	async def listReactionRoles(self, ctx):
-		"""List all of the reaction roles."""
+	@cmd_group.command(name="list")
+	@app_commands.checks.cooldown(2, 10, key=lambda i: (i.guild_id))
+	async def rr_list(self, interaction: discord.Interaction) -> None:
+		"""List reaction roles."""
 
-		guild_id = ctx.guild.id
-		guild_data = db.get_guild(guild_id)
+		await interaction.response.defer()
 
-		embed = discord.Embed(colour = discord.Colour.gold())
+		guild_data = await db.get_guild(interaction.guild.id)
 
-		embed.set_author(
-			name = f"Reaction Roles in {ctx.guild.name}",
-			icon_url = ctx.guild.icon.url
-		)
+		embed = discord.Embed(colour=discord.Colour.gold())
+		embed.set_author(name=f"Reaction Roles in {interaction.guild.id}", icon_url=interaction.guild.icon.url)
 
 		if not guild_data["reaction_roles"]:
-			embed.add_field(name = "None!", value = f"Use `{ctx.prefix}addReactionRole` to add some.")
+			embed.add_field(name="None!", value="Use `/reactionrole add` to add some.")
 		else:
 			for rr_id, rr_data in enumerate(guild_data["reaction_roles"]):
 				if rr_data["unicode_emoji"]:
 					emoji_object = rr_data["unicode_emoji"]
 				else:
-					emoji_object = await find_object.find_emoji(ctx.guild, rr_data["custom_emoji_id"])
+					emoji_object = await find_object.find_emoji(interaction.guild, rr_data["custom_emoji_id"])
 
-				channel_object = await find_object.find_channel(self.client, rr_data["channel_id"])
-				role_object = await find_object.find_role(ctx.guild, rr_data["role_id"])
+				channel_object = await find_object.find_channel(self.bot, rr_data["channel_id"])
+				role_object = await find_object.find_role(interaction.guild, rr_data["role_id"])
 
 				channel_str = channel_object and channel_object.mention or f":warning: ({str(rr_data['channel_id'])})"
 				emoji_str = emoji_object and str(emoji_object) or f":warning: ({str(rr_data['custom_emoji_id'])})"
 				role_str = role_object and role_object.mention or f":warning: ({str(rr_data['role_id'])})"
 
 				if channel_object:
-					jump_url = f"https://discord.com/channels/{guild_id}/{channel_object.id}/{rr_data['message_id']}"
+					jump_url = f"https://discord.com/channels/{interaction.guild.id}/{channel_object.id}/{rr_data['message_id']}"
 					embed_text = f"Channel: {channel_str}\nMessage ID: {rr_data['message_id']} [[Jump]]({jump_url})\nEmoji: {emoji_str}\nRole: {role_str}"
 				else:
 					embed_text = f"Channel: {channel_str}\nMessage ID: {rr_data['message_id']}\nEmoji: {emoji_str}\nRole: {role_str}"
@@ -121,79 +123,92 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 					value = embed_text
 				)
 
-		await ctx.send(embed=embed)
+		await interaction.followup.send(embed=embed)
 
-	@commands.command(aliases = ["createreactionrole", "addreactionrole"])
-	@commands.guild_only()
-	@commands.cooldown(1, 5, commands.BucketType.guild)
-	@checks.is_admin()
-	async def newReactionRole(self, ctx, channel: discord.TextChannel, message_id: int, emoji: Union[discord.Emoji, converters.UnicodeEmojiConverter], role: discord.Role):
-		"""Create a reaction role."""
+	@cmd_group.command(name="create")
+	@app_commands.checks.cooldown(2, 10, key=lambda i: (i.guild_id))
+	@app_commands.describe(
+		message="The message to react to.",
+		emoji="The emoji to react with.",
+		role="The role to add/remove."
+	)
+	async def rr_create(
+		self,
+		interaction: discord.Interaction,
+		message: app_commands.Transform[discord.Message, transformers.MessageTransformer], 
+		emoji: str,
+		role: app_commands.Transform[discord.Role, transformers.RoleTransformer]
+	) -> None:
+		"""Create a new reaction role."""
+
+		await interaction.response.defer()
 
 		try:
-			message = await channel.fetch_message(message_id)
-		except discord.HTTPException:
-			await ctx.send(f"❌ `{message_id}` is not a valid Message ID.")
-			return
+			await message.add_reaction(emoji)
+		except:
+			return await interaction.followup.send(embed=discord.Embed(description=f"❌ Invalid emoji: `{emoji}`.", colour=discord.Colour.red()))
 
-		await message.add_reaction(emoji)
-
-		guild_id = ctx.guild.id
-		guild_data = db.get_guild(guild_id)
+		guild_data = await db.get_guild(interaction.guild.id)
 
 		is_unicode_emoij = isinstance(emoji, str)
 		is_custom_emoji = isinstance(emoji, discord.Emoji)
 
 		guild_data["reaction_roles"].append({
-			"channel_id": channel.id,
-			"message_id": message_id,
+			"channel_id": message.channel.id,
+			"message_id": message.id,
 			"unicode_emoji": is_unicode_emoij and emoji or "",
 			"custom_emoji_id": is_custom_emoji and emoji.id or 0,
 			"role_id": role.id
 		})
 
-		db.set_guild(guild_id, guild_data)
+		await db.set_guild(interaction.guild.id, guild_data)
 
 		embed = discord.Embed(title = "✅ Reaction Role Successfully Created!", colour = discord.Colour.green())
 
 		embed.add_field(name = "ReactionRoleID: ", value = len(guild_data["reaction_roles"]) - 1, inline = False)
-		embed.add_field(name = "Channel: ", value = channel.mention, inline = False)
-		embed.add_field(name = "MessageId: ", value = message_id, inline = False)
+		embed.add_field(name = "Channel: ", value = message.channel.mention, inline = False)
+		embed.add_field(name = "MessageId: ", value = message.id, inline = False)
 		embed.add_field(name = "Emoji: ", value = str(emoji), inline = False)
 		embed.add_field(name = "Role: ", value = role.mention, inline = False)
 
-		await ctx.send(embed = embed)
+		await interaction.followup.send(embed=embed)
 
-	@commands.command(aliases = ["deletereactionrole"])
-	@commands.guild_only()
-	@commands.cooldown(1, 5, commands.BucketType.guild)
-	@checks.is_admin()
-	async def removeReactionRole(self, ctx, reactionRoleId: int):
+	@rr_create.autocomplete("role")
+	async def send_autocomplete(self, interaction: discord.Interaction, current_role: str) -> list[app_commands.Choice[str]]:
+		roles = interaction.guild.roles
+		return [app_commands.Choice(name=role.name, value=role.name) for role in roles if current_role in role.name and role.name != "@everyone"][:25]
+
+	@cmd_group.command(name="remove")
+	@app_commands.checks.cooldown(2, 10, key=lambda i: (i.guild_id))
+	@app_commands.describe(reaction_role_id="The reactionRoleID to remove.")
+	@app_commands.rename(reaction_role_id="reactionroleid")
+	async def rr_remove(
+		self,
+		interaction: discord.Interaction,
+		reaction_role_id: int
+	) -> None:
 		"""Remove a reaction role."""
 
-		guild_id = ctx.guild.id
-		guild_data = db.get_guild(guild_id)
+		await interaction.response.defer()
 
-		try:
-			guild_data["reaction_roles"][reactionRoleId]
-		except ValueError:
-			await ctx.send(f"❌ `{reactionRoleId}` is not a valid reaction role ID.")
-			return
+		guild_data = await db.get_guild(interaction.guild.id)
+		if reaction_role_id < 0 and reaction_role_id >= len(guild_data["reaction_roles"]):
+			return await interaction.followup.send(embed=discord.Embed(description=f"❌ Invalid reactionRoleID: `{reaction_role_id}`.", colour=discord.Colour.red()))
 
-		rr_data = guild_data["reaction_roles"].pop(reactionRoleId)
-		db.set_guild(guild_id, guild_data)
+		rr_data = guild_data["reaction_roles"].pop(reaction_role_id)
+		await db.set_guild(interaction.guild.id, guild_data)
 
 		if rr_data["unicode_emoji"]:
 			emoji_object = rr_data["unicode_emoji"]
 		else:
-			emoji_object = await find_object.find_emoji(ctx.guild, rr_data["custom_emoji_id"])
+			emoji_object = await find_object.find_emoji(interaction.guild, rr_data["custom_emoji_id"])
 
-		role_object = await find_object.find_role(ctx.guild, rr_data["role_id"])
-		channel_object = await find_object.find_channel(self.client, rr_data["channel_id"])
+		role_object = await find_object.find_role(interaction.guild, rr_data["role_id"])
+		channel_object = await find_object.find_channel(self.bot, rr_data["channel_id"])
 
 		try:
 			message_object = await channel_object.fetch_message(rr_data["message_id"])
-			await message_object.remove_reaction(emoji_object, self.client.user)
+			await message_object.remove_reaction(emoji_object, self.bot.user)
 		except:
 			pass
 
@@ -202,13 +217,18 @@ class reaction_roles(commands.Cog, name = "Reaction Roles"):
 			colour = discord.Colour.green()
 		)
 
-		embed.add_field(name = "ReactionRoleID: ", value = reactionRoleId, inline = False)
+		embed.add_field(name = "ReactionRoleID: ", value = reaction_role_id, inline = False)
 		embed.add_field(name = "Channel: ", value = channel_object and channel_object.mention or f":warning: ({str(rr_data['channel_id'])})", inline = False)
 		embed.add_field(name = "MessageId: ", value = rr_data["message_id"], inline = False)
 		embed.add_field(name = "Emoji: ", value = emoji_object and str(emoji_object) or f":warning: ({str(rr_data['custom_emoji_id'])})", inline = False)
 		embed.add_field(name = "Role: ", value = role_object and role_object.mention or f":warning: ({str(rr_data['role_id'])})", inline = False)
 
-		await ctx.send(embed = embed)
+		await interaction.followup.send(embed=embed)
 
-async def setup(client):
-	await client.add_cog(reaction_roles(client))
+	@rr_remove.autocomplete("reaction_role_id")
+	async def send_autocomplete(self, interaction: discord.Interaction, current_RRID: str) -> list[app_commands.Choice[str]]:
+		RRIDS = range(len((await db.get_guild(interaction.guild.id))["reaction_roles"]))
+		return [app_commands.Choice(name=str(RRID), value=str(RRID)) for RRID in RRIDS if current_RRID in str(RRID)]
+
+async def setup(bot: commands.Bot):
+	await bot.add_cog(reaction_roles(bot))
